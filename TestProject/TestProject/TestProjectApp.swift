ec43bde96b47b257d419120657f463dbc390ccae
Igor Kokoev <igor.kokoev@global.com>
2022-10-28 12:02:23 +0100

    Add TestProject

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Igor Kokoev on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
